b533dba11ed03868e9280e532928767da7c62d9c
Altered Art <cherokeedreemer@gmail.com>
2020-09-04 16:09:42 -0600

    adding state

---
+++ src/Card.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import robots from './robots'
+// import './card.styles.css';
+
+const Card = (props) => {
+    const {name, email, id} = props;
+    return (
+        
+
+        <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+        <img src={`https://robohash.org/${props.id}?200x200`} alt='robots'/>
+        <div>
+            <h2>{props.name}</h2>
+            <p>{props.email}</p>
+        </div>
+        </div>
+    )
+}
+
+export default Card;
