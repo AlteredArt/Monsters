8ecdebf48804e0ef98f15cebc982f028b5178a75
Altered Art <cherokeedreemer@gmail.com>
2020-09-05 01:07:50 -0600

    redux finished

--- src/index.js
+++ src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
 import 'tachyons';
+import thunkMiddleware from 'redux-thunk';
+import { Provider} from 'react-redux';
+import { createStore, applyMiddleware, combineReducers} from 'redux';
+import { searchRobots, requestRobots } from './reducers';
+import {createLogger} from 'redux-logger';
 
+const logger = createLogger();
+const rootReducers = combineReducers({searchRobots, requestRobots})
+const store = createStore(rootReducers, applyMiddleware(thunkMiddleware, logger))
 
 ReactDOM.render(
-
-    <App />, 
+    <Provider store={store}>
+      <App />
+    </Provider>,
   document.getElementById('root')
 );
